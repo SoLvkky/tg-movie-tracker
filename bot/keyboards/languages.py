021e71639040717eea067a5c532230fc0dc7cdb1
SoLukky <vlad.tirsky@gmail.com>
2026-01-20 16:54:22 -0500

    feat(i18n): add 3 new translations

--- bot/keyboards/languages.py
+++ bot/keyboards/languages.py
@@ -5,9 +5,9 @@ def get_lang_menu():
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [InlineKeyboardButton(text=t("language.set.en"), callback_data="lang:en-US")],
-            [InlineKeyboardButton(text=t("language.set.es"), callback_data="lang:none")],
-            [InlineKeyboardButton(text=t("language.set.fr"), callback_data="lang:none")],
-            [InlineKeyboardButton(text=t("language.set.de"), callback_data="lang:none")],
+            [InlineKeyboardButton(text=t("language.set.es"), callback_data="lang:es-ES")],
+            [InlineKeyboardButton(text=t("language.set.fr"), callback_data="lang:fr-FR")],
+            [InlineKeyboardButton(text=t("language.set.de"), callback_data="lang:de-DE")],
             [InlineKeyboardButton(text=t("language.set.ru"), callback_data="lang:ru-RU")],
             [InlineKeyboardButton(text=t("go_back"), callback_data="settings")]
         ],
